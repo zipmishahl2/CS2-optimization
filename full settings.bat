4d19aeb8273b0f6b1a086db5f8fbc5977c1be446
zipmishahl2 <110753825+zipmishahl2@users.noreply.github.com>
2024-04-02 21:15:09 +0700

    Update full settings.bat

--- full settings.bat
+++ full settings.bat
@@ -137,6 +137,7 @@ cls
 echo Making changes to the registry...
 echo.
 
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Power" /v "SleepReliabilityDetailedDiagnostics" /t REG_DWORD /d "0" /f
 reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Multimedia\SystemProfile" /v "AlwaysOn" /t REG_DWORD /d "1" /f
 reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Multimedia\SystemProfile\Tasks\Games" /v "GPU Priority" /t REG_DWORD /d "8" /f
 reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Multimedia\SystemProfile\Tasks\Games" /v "Priority" /t REG_DWORD /d "6" /f
