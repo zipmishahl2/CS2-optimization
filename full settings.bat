ac1ab7a3a0c657b2acdb36b57a158c3a1e501977
zipmishahl2 <110753825+zipmishahl2@users.noreply.github.com>
2024-04-04 15:15:18 +0700

    Update full settings.bat

--- full settings.bat
+++ full settings.bat
@@ -1,5 +1,5 @@
 @echo off
-set Version=2.1
+set Version=2.3
 break off
 color 03
 chcp 65001
@@ -19,11 +19,11 @@ echo                     ║╔══╝║║─────║║║║║╔�
 echo                     ║║───║╚═╗───║╚╝║║║─────║║──╔╝╚╗║║╚╝║║╔╝╚╗║─╚═╗║║║║──║║──╔╝╚╗║╚╝║║║─║║
 echo                     ╚╝───╚══╝───╚══╝╚╝─────╚╝──╚══╝╚╝──╚╝╚══╝╚═══╝╚╝╚╝──╚╝──╚══╝╚══╝╚╝─╚╝
 echo                  [1] Optimization                                                     [2] Services
-echo  ------------------------------------------------------------------------------------------------------------
-echo                                         Press function number (1-2)                                               
-echo  ------------------------------------------------------------------------------------------------------------
-echo                                           Version Batch: %Version%
-echo                                           Author: jamergamer_mod                                      
+echo                  [3] Network                                                          [4] Game priority 
+echo         ---------------------------------------------------------------------------------------------------
+echo                                         Press function number (1-4)                                               
+echo         ---------------------------------------------------------------------------------------------------
+echo                                           Version Batch: %Version%                                  
 choice /c 12345 /n
 set HomeSelection=%errorlevel%
 if %HomeSelection% == 1 (call :optimization)
@@ -521,7 +521,6 @@ reg add "HKCU\Software\Microsoft\GameBar" /v "GamePanelStartupTipIndex" /t REG_D
 reg add "HKCU\Software\Microsoft\GameBar" /v "ShowStartupPanel" /t REG_DWORD /d "0" /f
 reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Multimedia\SystemProfile" /v "NetworkThrottlingIndex" /t REG_DWORD /d "a" /f
 reg add "HKCU\Software\Microsoft\GameBar" /v "UseNexusForGameBarEnabled" /t REG_DWORD /d "0" /f
-reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\cs2.exe\PerfOptions" /v "CpuPriorityClass" /t REG_DWORD /d "8" /f
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\PriorityControl" /v "ConvertibleSlateMode" /t REG_DWORD /d "0" /f
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\PriorityControl" /v "Win32PrioritySeparation" /t REG_DWORD /d "2a" /f
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\AppCompat" /v "DisableInventory" /t REG_DWORD /d "1" /f
@@ -1629,51 +1628,51 @@ goto home
 
 :services
 echo disabled services windows...
-Set-Service AppVClient -StartupType Disabled
-Set-Service NetTcpPortSharing -StartupType Disabled
-Set-Service CscService -StartupType Disabled
-Set-Service PhoneSvc -StartupType Disabled
-Set-Service Spooler -StartupType Disabled
-Set-Service PrintNotify -StartupType Disabled
-Set-Service QWAVE -StartupType Disabled
-Set-Service RmSvc -StartupType Disabled
-Set-Service RemoteAccess -StartupType Disabled
-Set-Service SensorDataService -StartupType Disabled
-Set-Service SensrSvc -StartupType Disabled
-Set-Service SensorService -StartupType Disabled
-Set-Service ShellHWDetection -StartupType Disabled
-Set-Service SCardSvr -StartupType Disabled
-Set-Service ScDeviceEnum -StartupType Disabled
-Set-Service SSDPSRV -StartupType Disabled
-Set-Service WiaRpc -StartupType Disabled
-Set-Service upnphost -StartupType Disabled
-Set-Service UserDataSvc -StartupType Disabled
-Set-Service UevAgentService -StartupType Disabled
-Set-Service WalletService -StartupType Disabled
-Set-Service FrameServer -StartupType Disabled
-Set-Service stisvc -StartupType Disabled
-Set-Service wisvc -StartupType Disabled
-Set-Service icssvc -StartupType Disabled
-Set-Service WSearch -StartupType Disabled
-Set-Service XblAuthManager -StartupType Disabled
-Set-Service XblGameSave -StartupType Disabled
-Set-Service SEMgrSvc -StartupType Disabled
-Set-Service SysMain -StartupType Disabled
-Set-Service diagnosticshub.standardcollector.service -StartupType Disabled
-Set-Service diagsvc -StartupType Disabled
-Set-Service WbioSrvc -StartupType Disabled
-Set-Service MapsBloker -StartupType Disabled
-Set-Service lfsvc -StartupType Disabled
-Set-Service UevAgentService -StartupType Disabled
-Set-Service WinDefend -StartupType Disabled
-Set-Service SecurityHealthService -StartupType Disabled
-Set-Service WdNisSvc -StartupType Disabled
-Set-Service Sense -StartupType Disabled
-Set-Service wscsvc -StartupType Disabled
-Set-Service AxInstSV -StartupType Disabled
-Set-Service dmwappushservice -StartupType Disabled
-Set-Service SharedAccess -StartupType Disabled
-Set-Service lltdsvc -StartupType Disabled
+Powershell Set-Service AppVClient -StartupType Disabled
+Powershell Set-Service NetTcpPortSharing -StartupType Disabled
+Powershell Set-Service CscService -StartupType Disabled
+Powershell Set-Service PhoneSvc -StartupType Disabled
+Powershell Set-Service Spooler -StartupType Disabled
+Powershell Set-Service PrintNotify -StartupType Disabled
+Powershell Set-Service QWAVE -StartupType Disabled
+Powershell Set-Service RmSvc -StartupType Disabled
+Powershell Set-Service RemoteAccess -StartupType Disabled
+Powershell Set-Service SensorDataService -StartupType Disabled
+Powershell Set-Service SensrSvc -StartupType Disabled
+Powershell Set-Service SensorService -StartupType Disabled
+Powershell Set-Service ShellHWDetection -StartupType Disabled
+Powershell Set-Service SCardSvr -StartupType Disabled
+Powershell Set-Service ScDeviceEnum -StartupType Disabled
+Powershell Set-Service SSDPSRV -StartupType Disabled
+Powershell Set-Service WiaRpc -StartupType Disabled
+Powershell Set-Service upnphost -StartupType Disabled
+Powershell Set-Service UserDataSvc -StartupType Disabled
+Powershell Set-Service UevAgentService -StartupType Disabled
+Powershell Set-Service WalletService -StartupType Disabled
+Powershell Set-Service FrameServer -StartupType Disabled
+Powershell Set-Service stisvc -StartupType Disabled
+Powershell Set-Service wisvc -StartupType Disabled
+Powershell Set-Service icssvc -StartupType Disabled
+Powershell Set-Service WSearch -StartupType Disabled
+Powershell Set-Service XblAuthManager -StartupType Disabled
+Powershell Set-Service XblGameSave -StartupType Disabled
+Powershell Set-Service SEMgrSvc -StartupType Disabled
+Powershell Set-Service SysMain -StartupType Disabled
+Powershell Set-Service diagnosticshub.standardcollector.service -StartupType Disabled
+Powershell Set-Service diagsvc -StartupType Disabled
+Powershell Set-Service WbioSrvc -StartupType Disabled
+Powershell Set-Service MapsBloker -StartupType Disabled
+Powershell Set-Service lfsvc -StartupType Disabled
+Powershell Set-Service UevAgentService -StartupType Disabled
+Powershell Set-Service WinDefend -StartupType Disabled
+Powershell Set-Service SecurityHealthService -StartupType Disabled
+Powershell Set-Service WdNisSvc -StartupType Disabled
+Powershell Set-Service Sense -StartupType Disabled
+Powershell Set-Service wscsvc -StartupType Disabled
+Powershell Set-Service AxInstSV -StartupType Disabled
+Powershell Set-Service dmwappushservice -StartupType Disabled
+Powershell Set-Service SharedAccess -StartupType Disabled
+Powershell Set-Service lltdsvc -StartupType Disabled
 timeout /t 2 /nobreak > NUL
 timeout /t 1 /nobreak > NUL
 echo Need a reboot, restart pc now or later?
@@ -1686,6 +1685,203 @@ choice /c 12 /n
 set OptimizationSelection=%errorlevel%
 if %OptimizationSelection% == 1 (cls & echo Close all applications to make your PC reboot faster, you have 10 second. & shutdown /r /t 10)
 if %OptimizationSelection% == 2 (goto home)
+goto home
+
+:network
+:: Adjust Sock Address Size
+echo Configuring Sock Address Size
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters\Winsock" /v "MinSockAddrLength" /t REG_DWORD /d "16" /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters\Winsock" /v "MaxSockAddrLength" /t REG_DWORD /d "16" /f
+timeout /t 1 /nobreak > NUL
+
+:: Disable Nagle's Algorithm
+echo Disabling Nagle's Algorithm
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters\Interfaces" /v "TcpAckFrequency" /t REG_DWORD /d "1" /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters\Interfaces" /v "TCPNoDelay" /t REG_DWORD /d "1" /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters\Interfaces" /v "TcpDelAckTicks" /t REG_DWORD /d "0" /f
+timeout /t 1 /nobreak > NUL
+
+:: Disable Delivery Optimization
+echo Disabling Delivery Optimization
+reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\DeliveryOptimization\Config" /v "DODownloadMode" /t REG_DWORD /d "0" /f
+reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\DeliveryOptimization\Config" /v "DownloadMode" /t REG_DWORD /d "0" /f
+reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\DeliveryOptimization\Settings" /v "DownloadMode" /t REG_DWORD /d "0" /f
+timeout /t 1 /nobreak > NUL
+
+:: Limit Number of SMB Sessions
+echo Limiting SMB Sessions
+reg add "HKLM\SYSTEM\CurrentControlSet\services\LanmanServer\Parameters" /v "Size" /t REG_DWORD /d "3" /f
+timeout /t 1 /nobreak > NUL
+
+:: Disable Oplocks
+echo Disabling Oplocks
+reg add "HKLM\SYSTEM\CurrentControlSet\services\LanmanServer\Parameters" /v "EnableOplocks" /t REG_DWORD /d "0" /f
+timeout /t 1 /nobreak > NUL
+
+:: Set IRP Stack Size
+echo Setting IRP Stack Size
+reg add "HKLM\SYSTEM\CurrentControlSet\services\LanmanServer\Parameters" /v "IRPStackSize" /t REG_DWORD /d "20" /f
+timeout /t 1 /nobreak > NUL
+
+:: Disable Sharing Violations
+echo Disabling Sharing Violations
+reg add "HKLM\SYSTEM\CurrentControlSet\services\LanmanServer\Parameters" /v "SharingViolationDelay" /t REG_DWORD /d "0" /f
+reg add "HKLM\SYSTEM\CurrentControlSet\services\LanmanServer\Parameters" /v "SharingViolationRetries" /t REG_DWORD /d "0" /f
+timeout /t 1 /nobreak > NUL
+
+:: Get the Sub ID of the Network Adapter
+for /f %%n in ('Reg query "HKLM\SYSTEM\CurrentControlSet\Control\Class\{4D36E972-E325-11CE-BFC1-08002bE10318}" /v "*SpeedDuplex" /s ^| findstr  "HKEY"') do (
+
+:: Disable NIC Power Savings
+echo Disabling NIC Power Savings
+reg add "%%n" /v "AutoPowerSaveModeEnabled" /t REG_SZ /d "0" /f 
+reg add "%%n" /v "AutoDisableGigabit" /t REG_SZ /d "0" /f 
+reg add "%%n" /v "AdvancedEEE" /t REG_SZ /d "0" /f 
+reg add "%%n" /v "DisableDelayedPowerUp" /t REG_SZ /d "2" /f 
+reg add "%%n" /v "*EEE" /t REG_SZ /d "0" /f 
+reg add "%%n" /v "EEE" /t REG_SZ /d "0" /f 
+reg add "%%n" /v "EnablePME" /t REG_SZ /d "0" /f
+reg add "%%n" /v "EEELinkAdvertisement" /t REG_SZ /d "0" /f 
+reg add "%%n" /v "EnableGreenEthernet" /t REG_SZ /d "0" /f 
+reg add "%%n" /v "EnableSavePowerNow" /t REG_SZ /d "0" /f 
+reg add "%%n" /v "EnablePowerManagement" /t REG_SZ /d "0" /f
+reg add "%%n" /v "EnableDynamicPowerGating" /t REG_SZ /d "0" /f
+reg add "%%n" /v "EnableConnectedPowerGating" /t REG_SZ /d "0" /f
+reg add "%%n" /v "EnableWakeOnLan" /t REG_SZ /d "0" /f 
+reg add "%%n" /v "GigaLite" /t REG_SZ /d "0" /f
+reg add "%%n" /v "NicAutoPowerSaver" /t REG_SZ /d "2" /f 
+reg add "%%n" /v "PowerDownPll" /t REG_SZ /d "0" /f
+reg add "%%n" /v "PowerSavingMode" /t REG_SZ /d "0" /f 
+reg add "%%n" /v "ReduceSpeedOnPowerDown" /t REG_SZ /d "0" /f 
+reg add "%%n" /v "SmartPowerDownEnable" /t REG_SZ /d "0" /f 
+reg add "%%n" /v "S5NicKeepOverrideMacAddrV2" /t REG_SZ /d "0" /f
+reg add "%%n" /v "S5WakeOnLan" /t REG_SZ /d "0" /f 
+reg add "%%n" /v "ULPMode" /t REG_SZ /d "0" /f 
+reg add "%%n" /v "WakeOnDisconnect" /t REG_SZ /d "0" /f
+reg add "%%n" /v "*WakeOnMagicPacket" /t REG_SZ /d "0" /f
+reg add "%%n" /v "*WakeOnPattern" /t REG_SZ /d "0" /f
+reg add "%%n" /v "WakeOnLink" /t REG_SZ /d "0" /f 
+reg add "%%n" /v "WolShutdownLinkSpeed" /t REG_SZ /d "2" /f
+timeout /t 1 /nobreak > NUL
+
+:: Disable Jumbo Frame
+echo Disabling Jumbo Frame
+reg add "%%n" /v "JumboPacket" /t REG_SZ /d "1514" /f
+timeout /t 1 /nobreak > NUL
+
+:: Configure Receive/Transmit Buffers
+echo Configuring Buffer Sizes
+reg add "%%n" /v "TransmitBuffers" /t REG_SZ /d "4096" /f
+reg add "%%n" /v "ReceiveBuffers" /t REG_SZ /d "512" /f
+timeout /t 1 /nobreak > NUL
+
+:: Configure Offloads
+echo Configuring Offloads
+reg add "%%n" /v "IPChecksumOffloadIPv4" /t REG_SZ /d "0" /f 
+reg add "%%n" /v "LsoV1IPv4" /t REG_SZ /d "0" /f 
+reg add "%%n" /v "LsoV2IPv4" /t REG_SZ /d "0" /f 
+reg add "%%n" /v "LsoV2IPv6" /t REG_SZ /d "0" /f 
+reg add "%%n" /v "PMARPOffload" /t REG_SZ /d "0" /f
+reg add "%%n" /v "PMNSOffload" /t REG_SZ /d "0" /f 
+reg add "%%n" /v "TCPChecksumOffloadIPv4" /t REG_SZ /d "0" /f 
+reg add "%%n" /v "TCPChecksumOffloadIPv6" /t REG_SZ /d "0" /f 
+reg add "%%n" /v "UDPChecksumOffloadIPv6" /t REG_SZ /d "0" /f 
+reg add "%%n" /v "UDPChecksumOffloadIPv4" /t REG_SZ /d "0" /f 
+timeout /t 1 /nobreak > NUL
+
+:: Enable RSS in NIC
+echo Enabling RSS in NIC
+reg add "%%n" /v "RSS" /t REG_SZ /d "1" /f
+reg add "%%n" /v "*NumRssQueues" /t REG_SZ /d "2" /f
+reg add "%%n" /v "RSSProfile" /t REG_SZ /d "3" /f
+timeout /t 1 /nobreak > NUL
+
+:: Disable Flow Control
+echo Disabling Flow Control
+reg add "%%n" /v "*FlowControl" /t REG_SZ /d "0" /f
+reg add "%%n" /v "FlowControlCap" /t REG_SZ /d "0" /f
+timeout /t 1 /nobreak > NUL
+
+:: Remove Interrupt Delays
+echo Removing Interrupt Delays
+reg add "%%n" /v "TxIntDelay" /t REG_SZ /d "0" /f 
+reg add "%%n" /v "TxAbsIntDelay" /t REG_SZ /d "0" /f 
+reg add "%%n" /v "RxIntDelay" /t REG_SZ /d "0" /f 
+reg add "%%n" /v "RxAbsIntDelay" /t REG_SZ /d "0" /f
+timeout /t 1 /nobreak > NUL
+
+:: Remove Adapter Notification
+echo Removing Adapter Notification Sending
+reg add "%%n" /v "FatChannelIntolerant" /t REG_SZ /d "0" /f
+timeout /t 1 /nobreak > NUL
+
+:: Disable Interrupt Moderation
+echo Disabling Interrupt Moderation
+reg add "%%n" /v "*InterruptModeration" /t REG_SZ /d "0" /f
+timeout /t 1 /nobreak > NUL
+)
+
+:: Enable WeakHost Send and Recieve (melodytheneko)
+echo Enabling WH Send and Recieve
+powershell "Get-NetAdapter -IncludeHidden | Set-NetIPInterface -WeakHostSend Enabled -WeakHostReceive Enabled -ErrorAction SilentlyContinue"
+timeout /t 1 /nobreak > NUL
+
+goto home
+
+:gamepriority
+set z=[7m
+set i=[1m
+set q=[0m
+echo %z%Are you on cs2, valorant fortnite?%q%
+echo.
+echo %i%cs2 = 1%q%
+echo.
+echo %i%valorant = 2%q%
+echo.
+echo %i%fortnite = 3%q%
+echo.
+set choice=
+set /p choice=
+if not '%choice%'=='' set choice=%choice:~0,1%
+if '%choice%'=='1' goto cs2
+if '%choice%'=='2' goto valorant
+if '%choice%'=='3' goto fortnite
+
+:cs2
+cls
+title priority cs2..
+reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\cs2.exe\PerfOptions" /v "CpuPriorityClass" /t REG_DWORD /d "8" /f
+echo Time to drag in the game!
+echo Wait 2 second...
+timeout /t 1 /nobreak >nul
+cls
+echo Wait 1 second...
+timeout /t 1 /nobreak >nul
+goto home
+
+:valorant
+cls
+title priority valorant..
+reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\VALORANT.exe\PerfOptions" /v "CpuPriorityClass" /t REG_DWORD /d "8" /f
+echo Time to drag in the game!
+echo Wait 2 second...
+timeout /t 1 /nobreak >nul
+cls
+echo Wait 1 second...
+timeout /t 1 /nobreak >nul
+goto home
+
+:fortnite
+cls
+title priority fortnite..
+reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\fortnite.exe\PerfOptions" /v "CpuPriorityClass" /t REG_DWORD /d "3" /f
+reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\FortniteClient-Win64-Shipping.exe" /v "CpuPriorityClass" /t REG_DWORD /d "3" /f
+echo Time to drag in the game!
+echo Wait 2 second...
+timeout /t 1 /nobreak >nul
+cls
+echo Wait 1 second...
+timeout /t 1 /nobreak >nul
 goto home
 
 :adminwindow
